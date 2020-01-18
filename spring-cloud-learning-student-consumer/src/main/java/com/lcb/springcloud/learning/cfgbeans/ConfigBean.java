5bf4f53324acb006d82f2027c112596164fa155e
yuexin <610696148@qq.com>
2020-01-18 11:48:18 +0800

    Eureka集群

--- spring-cloud-learning-student-consumer/src/main/java/com/lcb/springcloud/learning/cfgbeans/ConfigBean.java
+++ spring-cloud-learning-student-consumer/src/main/java/com/lcb/springcloud/learning/cfgbeans/ConfigBean.java
@@ -1,5 +1,6 @@
 package com.lcb.springcloud.learning.cfgbeans;
 
+import com.netflix.loadbalancer.IRule;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,8 +10,11 @@ import org.springframework.web.client.RestTemplate;
 public class ConfigBean {
     @Bean
     @LoadBalanced  //开启Ribbon负责均衡，Ribbon是基于Netfix Ribbon实现的一客户端负载均衡工具
-    public RestTemplate getRestTemplate()
-    {
+    public RestTemplate getRestTemplate() {
         return new RestTemplate();
+    }
+
+    public IRule myRule() {
+        return null;
     }
 }
